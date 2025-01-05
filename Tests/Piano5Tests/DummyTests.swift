8871a4f9aa8a037365a7a74f1321829519775c06
bryandebourbon <bryandebourbon@gmail.com>
2025-01-05 14:33:54 -0500

    Add/update file Tests/Piano5Tests/DummyTests.swift

---
+++ Tests/Piano5Tests/DummyTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Piano5
+
+final class DummyTests: XCTestCase {
+    func testExample() throws {
+        // This is a dummy test so SwiftPM sees a Tests target.
+        XCTAssertTrue(true)
+    }
+}
